99ef00001651092639e59338d75e0b799ada5b6a
Tamara Berrocal <tamaraberrocal@gmail.com>
2021-06-21 15:00:39 -0300

    Fix dependencies

--- models/Doggo.js
+++ models/Doggo.js
@@ -4,20 +4,20 @@ const Schema = mongoose.Schema;
 const DoggoSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   type: {
     type: String,
-    required: true
+    required: true,
   },
   isGoodBoi: {
     type: Boolean,
-    default: true
+    default: true,
   },
   spottedDate: {
     type: Date,
-    default: Date.now()
-  }
+    default: Date.now(),
+  },
 });
 
 module.exports = Item = mongoose.model('doggo', DoggoSchema);
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 const config = require('config');
 
 const app = express();
 
 /* Enable CORS */
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
@@ -17,19 +19,23 @@ const db = config.get('mongoURI');
 
 /* Connect to DB */
 mongoose
-  .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log('MongoDB is connected'))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 /* Get Mongoose schema */
 const Doggo = require('./models/Doggo');
 
 /* RESTful API METHODS */
 //GET
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   Doggo.find()
     .sort({ date: -1 })
-    .then(items => res.json(items));
+    .then((items) => res.json(items));
 });
 
 //POST
@@ -40,38 +46,44 @@ app.post('/', (req, res) => {
     name: req.body.name,
     type: req.body.type,
     isGoodBoi: req.body.isGoodBoi,
-  })
+  });
 
   doggo
     .save()
-    .then(item => res.json(item))
-    .catch(err => res.send(err));
+    .then((item) => res.json(item))
+    .catch((err) => res.send(err));
 });
 
 //PUT
 app.put('/:name', (req, res) => {
   Doggo.findOneAndUpdate({ name: req.params.name }, req.body)
-    //.then(() => res.json({ success: true }))
-    .then(item => {
+    .then((item) => {
       let itemUpd = item;
 
-      itemUpd.name !== req.body.name ? itemUpd.name = req.body.name : false;
-      itemUpd.type !== req.body.type ? itemUpd.type = req.body.type : false;
-      itemUpd.isGoodBoi !== req.body.isGoodBoi ? itemUpd.isGoodBoi = req.body.isGoodBoi : false;
+      itemUpd.name !== req.body.name ? (itemUpd.name = req.body.name) : false;
+      itemUpd.type !== req.body.type ? (itemUpd.type = req.body.type) : false;
+      itemUpd.isGoodBoi !== req.body.isGoodBoi
+        ? (itemUpd.isGoodBoi = req.body.isGoodBoi)
+        : false;
 
       res.json(itemUpd);
     })
-    .catch(err => res.status(404).json({ success: false }));
+    .catch(() => res.status(404).json({ success: false }));
 });
 
 //DELETE
 app.delete('/:name', (req, res) => {
   Doggo.findOneAndDelete({ name: req.params.name })
-    //.then(() => res.json({ success: true }))
-    .then(item => item === null ? res.status(404).json({ success: false }) : res.send(req.params.name+" is gone"))
-    .catch(err => res.status(404).json({ success: false }));
+    .then((item) =>
+      item === null
+        ? res.status(404).json({ success: false })
+        : res.send(req.params.name + ' is gone')
+    )
+    .catch(() => res.status(404).json({ success: false }));
 });
 
 /* Listen from port 5000 */
 const port = 5000;
-app.listen(port, () => console.log(`Server started on port: http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server started on port: http://localhost:${port}`)
+);
